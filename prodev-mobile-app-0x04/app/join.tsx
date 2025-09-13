cc04b2df897b73e7f94ab4a5333f05425e58b371
ay981 <aymenabdulkerim411@gmail.com>
2025-09-13 21:49:00 +0300

    Add initial layout, screens, constants, and styles for the app

---
+++ prodev-mobile-app-0x04/app/join.tsx
@@ -0,0 +1,10 @@
+// TODO: Move your previous join screen code here
+import { View, Text } from 'react-native';
+
+export default function JoinScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Join Screen</Text>
+    </View>
+  );
+}
---
+++ prodev-mobile-app-0x04/app/signin.tsx
@@ -0,0 +1,10 @@
+// TODO: Move your previous signin screen code here
+import { View, Text } from 'react-native';
+
+export default function SigninScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Signin Screen</Text>
+    </View>
+  );
+}
